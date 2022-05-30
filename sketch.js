9a7fb9ec64dbd0f724bf08b80bc6453a0d0b17cc
Poker Lamp <93736740+JoseLohan@users.noreply.github.com>
2022-05-30 11:58:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var tim;
+var astronomyBook;
+var balcony, bathroom, bedroom, courtyard, garden, kitchen, livingroom, timBedroom;
+var earthST, jupterST, marsST, mercuryST, neptuneST, saturnST, uranusST, venusST;
+
+
+function preload(){
+    tim.loadImage("./images/tim.png");
+    astronomyBook.loadImage("./images/astronomyBook.jpg");
+
+    balcony.loadImage("./backgroundImages/balconyBackground.png");
+    timBedroom.loadImage("./backgroundImages/timBedroomBackground.png");
+    bedroom.loadImage("./backgroundImages/bedroomBackground.png");
+    bathroom.loadImage("./backgroundImages/bathroomBackground.png");
+    kitchen.loadImage("./backgroundImages/kitchenBackground.png");
+    garden.loadImage("./backgroundImages/gardenBackground.png");
+    courtyard.loadImage("./backgroundImages/courtyardBackground.png");
+    livingroom.loadImage("./backgroundImages/livingroomBackground.png");
+    
+    mercuryST.loadImage("./stickersImages/mercurySticker.png");
+    venusST.loadImage("./stickersImages/venusSticker.png");
+    earthST.loadImage("./stickersImages/earthSticker.png");
+    marsST.loadImage("./stickersImages/marsSticker.png");
+    jupterST.loadImage("./stickersImages/jupterSticker.png");
+    saturnST.loadImage("./stickersImages/saturnSticker.png");
+    uranusST.loadImage("./stickersImages/uranusSticker.png");
+    neptuneST.loadImage("./stickersImages/neptuneSticker.png");
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+ 
+}
